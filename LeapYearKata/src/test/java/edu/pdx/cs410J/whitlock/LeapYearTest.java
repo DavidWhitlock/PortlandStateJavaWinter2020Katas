6cc719028dbda81c35129977b08c7cb7ac01a468
David Whitlock <david.m.whitlock@gmail.com>
2020-01-22 18:36:06 -0800

    Got rid of rest of Student code.  Now it's time to write the Kata!

--- LeapYearKata/src/test/java/edu/pdx/cs410J/whitlock/LeapYearTest.java
+++ LeapYearKata/src/test/java/edu/pdx/cs410J/whitlock/LeapYearTest.java
@@ -2,11 +2,6 @@ package edu.pdx.cs410J.whitlock;
 
 import org.junit.Test;
 
-import java.util.ArrayList;
-
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.core.IsEqual.equalTo;
-
 /**
  * Unit tests for the Student class.  In addition to the JUnit annotations,
  * they also make use of the <a href="http://hamcrest.org/JavaHamcrest/">hamcrest</a>
@@ -16,10 +11,7 @@ public class LeapYearTest
 {
 
   @Test
-  public void studentNamedPatIsNamedPat() {
-    String name = "Pat";
-    var pat = new LeapYear(name, new ArrayList<>(), 0.0, "Doesn't matter");
-    assertThat(pat.getName(), equalTo(name));
-  }
+  public void firstLeapYearTest() {
 
+  }
 }
