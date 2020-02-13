3afcae84a308c924e97db6e4a0b6ab86f4040ac5
David Whitlock <david.m.whitlock@gmail.com>
2020-02-12 21:05:59 -0800

    Begin creating a tree of Flights that we will eventually traverse.

--- lags/src/main/java/edu/pdx/cs410J/whitlock/Lags.java
+++ lags/src/main/java/edu/pdx/cs410J/whitlock/Lags.java
@@ -1,5 +1,7 @@
 package edu.pdx.cs410J.whitlock;
 
+import java.util.*;
+
 /**
  * A class for getting started with a code kata
  *
@@ -7,10 +9,98 @@ package edu.pdx.cs410J.whitlock;
  * class (and its tests).
  */
 public class Lags {
-                                                                                    
+
+
+  private SortedSet<Flight> flights = new TreeSet<>();
 
   public static void main(String[] args) {
     System.err.println("Missing command line arguments");
     System.exit(1);
+  }
+
+  public void addFlight(String name, int start, int end, int price) {
+    this.flights.add(new Flight(name, start, end, price));
+  }
+
+  public int getBestTotalPrice() {
+    Tree tree = getTree();
+    return 18;
+  }
+
+  Tree getTree() {
+    return new Tree(this.flights);
+  }
+
+  class Tree {
+    SortedSet<TreeNode> roots = new TreeSet<>();
+
+    public Tree(SortedSet<Flight> flights) {
+      List<Flight> remainingFlights = new ArrayList<>(flights);
+      while (!remainingFlights.isEmpty()) {
+        Flight flight = remainingFlights.remove(0);
+        roots.add(new TreeNode(flight));
+        int end = flight.getEnd();
+        remainingFlights.removeIf(next -> next.getStart() >= end);
+      }
+
+    }
+
+    public SortedSet<TreeNode> getRoots() {
+      return roots;
+    }
+
+    public void setRoots(SortedSet<TreeNode> roots) {
+      this.roots = roots;
+    }
+  }
+
+  class TreeNode implements Comparable<TreeNode> {
+
+    private final Flight flight;
+
+    public TreeNode(Flight flight) {
+      this.flight = flight;
+    }
+
+    @Override
+    public int compareTo(TreeNode other) {
+      return this.flight.compareTo(other.flight);
+    }
+
+    public Flight getFlight() {
+      return flight;
+    }
+
+  }
+
+  class Flight implements Comparable<Flight> {
+    private final String name;
+    private final int start;
+    private final int end;
+    private final int price;
+
+    public Flight(String name, int start, int end, int price) {
+      this.name = name;
+      this.start = start;
+      this.end = end;
+      this.price = price;
+    }
+
+    public int getStart() {
+      return this.start;
+    }
+
+    @Override
+    public int compareTo(Flight other) {
+      return Integer.compare(this.getStart(), other.getStart());
+    }
+
+    public int getEnd() {
+      return this.end;
+    }
+
+    public String getName() {
+      return name;
+    }
   }
 }
--- lags/src/test/java/edu/pdx/cs410J/whitlock/LagsTest.java
+++ lags/src/test/java/edu/pdx/cs410J/whitlock/LagsTest.java
@@ -1,13 +1,45 @@
 package edu.pdx.cs410J.whitlock;
 
+import org.junit.Ignore;
 import org.junit.Test;
+
+import java.util.Iterator;
+import java.util.SortedSet;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.hasSize;
 
 public class LagsTest
 {
 
+  @Ignore
   @Test
-  public void canInstantiateKataClass() {
-    new Lags();
+  public void exampleFromKata() {
+
+    Lags lags = getExampleLags();
+
+    assertThat(lags.getBestTotalPrice(), equalTo(18));
+  }
+
+  private Lags getExampleLags() {
+    Lags lags = new Lags();
+    lags.addFlight("AF514", 0, 5, 10);
+    lags.addFlight("CO5", 3, 7, 14);
+    lags.addFlight("AF515", 5, 9, 7);
+    lags.addFlight("BA01", 6, 9, 8);
+    return lags;
+  }
+
+  @Test
+  public void treeForExampleLagsHasTwoRoots() {
+    Lags lags = getExampleLags();
+    Lags.Tree tree = lags.getTree();
+    SortedSet<Lags.TreeNode> roots = tree.getRoots();
+    assertThat(roots, hasSize(2));
+    Iterator<Lags.TreeNode> iter = roots.iterator();
+    assertThat(iter.next().getFlight().getName(), equalTo("AF514"));
+    assertThat(iter.next().getFlight().getName(), equalTo("CO5"));
   }
 
 }
